80cfb00d9799b7a163ec8d22becad964e611872a
Baaba-g <imransalara31@gmail.com>
2024-08-08 00:23:31 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923416564211"
+global.ownername = "🐛 MR Imran "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
