7c8bb19cc6e126e82710ddea0d29dbaba5701242
Baaba-g <imransalara31@gmail.com>
2024-08-11 23:45:22 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,13 +3,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923273232558"
+global.ownername = "🐛 MR IMRAN "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
+global.ownernumber = '923012600757'  //creator number
 global.ownername = ' MR WASI ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
